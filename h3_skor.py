a45d0da024c2753906dd6c413ccd23d9a6b259dc
mertcankati <mertcankati0@gmail.com>
2025-03-03 15:25:41 +0300

    Update h3_skor.py

---
+++ h3_skor.py
@@ -0,0 +1,15 @@
+import random 
+gala= random.randint(1,10)
+print(gala)
+fener= random.randint(1,10)
+print(fener)
+print("galatasaray puanı")
+print(gala)
+print("fener puanı")
+print(fener)
+if gala>fener :
+  print("galatasaray galip")
+elif gala==fener :
+  print("berabere")
+  else 
+  print("fener galip")
